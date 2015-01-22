2686ea08cc96916d2524f961be5cc5f9d5712e5b
Danny J <Josizzle619@yahoo.com>
2015-01-21 23:58:35 -0500

    message

--- Bootstrap/bootstrap.js
+++ Bootstrap/bootstrap.js
@@ -1,1 +1,9 @@
-bootstrap.js
+Use Strict
+var DannyJ = document.getElementbyID('DannyJ');
+console.log("DannyJ");
+var function "Hello DannyJ"
+document.getElementbyID("DannyJ") style.background="blue";
+DannyJ.addeventlistener ("click", myfunction);
+function my function() {
+	alert ("Hello There Human");
+}
