f18f44a662f380781ce6b5e158326f7a2b0a589a
Danny J <Josizzle619@yahoo.com>
2015-02-05 01:04:52 -0500

    message

--- Resume/js/app.js
+++ Resume/js/app.js
@@ -1,5 +1,4 @@
-"use strict"
-
+"use strict";
 
 $(document).ready(function() {
 
@@ -11,6 +10,26 @@ $(document).ready(function() {
     });
 
     // wow.min.js code //
+
     new WOW().init();
 
+// countTo code //
+ $('.timer').countTo({
+ 	from: 0,
+ 	to: 32,
+ 	speed: 5000,
+ 	reFreshInterval: 50,
+ 	})
+ $('.timed').countTo({
+ 	from: 0,
+ 	to: 1000,
+ 	speed: 3500,
+ 	reFreshInterval: 50,
+ })
+ $('.times').countTo({
+ 	from: 0,
+ 	to: 300,
+ 	speed: 3000,
+ 	reFreshInterval: 50,
+ })
 });
