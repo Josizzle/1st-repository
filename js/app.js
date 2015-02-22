98781d08418ed2bbd3b0fa7120b174e886878bdd
Danny J <Josizzle619@yahoo.com>
2015-02-22 14:51:39 -0500

    Added Sass to resume site. Needs more updates

---
+++ js/app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+$(document).ready(function() {
+
+    // text rotator code //
+    $(".rotate").textrotator({
+        animation: "dissolve",
+        seperator: ",",
+        speed: 2000,
+    });
+
+    // wow.min.js code //
+
+    new WOW().init();
+
+    // countTo code //
+    $('.timer').countTo({
+        from: 0,
+        to: 32,
+        speed: 5000,
+        reFreshInterval: 50,
+    });
+    $('.timed').countTo({
+        from: 0,
+        to: 1000,
+        speed: 3500,
+        reFreshInterval: 50,
+    });
+    $('.times').countTo({
+        from: 0,
+        to: 300,
+        speed: 3000,
+        reFreshInterval: 50,
+    });
+});
