0b2beda22fd956b117887b781edad76f93143869
Danny J <Josizzle619@yahoo.com>
2015-02-23 16:09:26 -0500

    Resume updated with Sass

--- Resume/js/app.js
+++ Resume/js/app.js
@@ -2,6 +2,17 @@
 
 $(document).ready(function() {
 
+    // owl carousel
+    $("#owl-demo").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+        items: 4,
+        // itemsDesktop : [1199,3],
+        // itemsDesktopSmall : [979,3]
+
+    });
+
     // text rotator code //
     $(".rotate").textrotator({
         animation: "dissolve",
