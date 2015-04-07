9b23005e493b27493b4f3a731db92817946d22b2
Josizzle <Josizzle619@yahoo.com>
2015-04-06 22:48:28 -0400

    added oriDomi effects to portfolio

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,14 +1,31 @@
 'use strict'
 $(document).ready(function() {
-	// call owl-carousel lower images
-  $("#owl-demo").owlCarousel({
- 
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
- 
-      items : 4,
-      // itemsDesktop : [1199,3],
-      // itemsDesktopSmall : [979,3]
- 
-  });
- 
+    // call owl-carousel lower images
+    $("#owl-demo").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+        items: 4,
+
+    });
+    // oriDomi using Jquery
+    var $folded = $('.graphart').oriDomi({
+        vPanels: 5,
+
+    });
+    // oriDomi effect
+    $folded.oriDomi('twist', 20);
+
+    // Call for oriDomi effect using Javascript
+    var folded = new OriDomi('.grandMotel', {
+        vPanels: 10,
+        ripple: true,
+
+    });
+
+  
+
+
+
+
 });
